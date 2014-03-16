6b6c51bc9665166241b7524793cf67d1d3abef30
bobscott78 <bobscott78@gmail.com>
2014-03-16 19:43:20 +0000

    Basic types

---
+++ RxInvestigation/KeyTypes.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace RxInvestigation
+{
+    [TestFixture]
+    public class KeyTypes
+    {
+        [Test]
+        public void SubjectExample()
+        {
+            var subject = new Subject<string>();
+            var observations = new StringBuilder();
+
+            subject.OnNext("a");
+            
+            subject.Subscribe(s => observations.Append(s));
+
+            subject.OnNext("b");
+            subject.OnNext("c");
+
+            Assert.That(observations.ToString(), Is.EqualTo("bc"));
+        }
+
+        [Test]
+        public void ReplaySubjectExample()
+        {
+            var subject = new ReplaySubject<string>(2);
+            var observations = new StringBuilder();
+
+            subject.OnNext("a");
+            subject.OnNext("b");
+            subject.OnNext("c");
+            
+            subject.Subscribe(s => observations.Append(s));
+
+            subject.OnNext("d");
+
+            Assert.That(observations.ToString(), Is.EqualTo("bcd"));            
+        }
+
+        [Test]
+        public void ReplaySubjectWindowExample()
+        {
+            var window = TimeSpan.FromMilliseconds(150);
+            var subject = new ReplaySubject<string>(window);
+            var observations = new StringBuilder();
+
+            subject.OnNext("a");
+            Thread.Sleep(100);
+            subject.OnNext("b");
+            Thread.Sleep(100);
+
+            subject.Subscribe(s => observations.Append(s));
+
+            subject.OnNext("c");
+
+            Assert.That(observations.ToString(), Is.EqualTo("bc"));
+        }
+
+        [Test]
+        public void BehaviorSubjectWithNoNext()
+        {
+            var subject = new BehaviorSubject<string>("a");
+            var observations = new StringBuilder();
+            
+            subject.Subscribe(s => observations.Append(s));
+
+            Assert.That(observations.ToString(), Is.EqualTo("a"));
+        }
+
+        [Test]
+        public void BehaviorSubjectWithNext()
+        {
+            var subject = new BehaviorSubject<string>("a");
+            var observations = new StringBuilder();
+            
+            subject.OnNext("b");
+            subject.OnNext("c");
+            subject.Subscribe(s => observations.Append(s));
+            subject.OnNext("d");
+
+            Assert.That(observations.ToString(), Is.EqualTo("cd"));
+        }
+
+        [Test]
+        public void BehaviorSubjectCompleted()
+        {
+            var subject = new BehaviorSubject<string>("a");
+            var observations = new StringBuilder();
+
+            subject.OnNext("b");
+            subject.OnNext("c");
+            subject.OnCompleted();
+            subject.Subscribe(s => observations.Append(s));
+            
+            Assert.That(observations.ToString(), Is.EqualTo(string.Empty));
+        }
+
+        [Test]
+        public void AsyncSubjectNotCompleted()
+        {
+            var subject = new AsyncSubject<string>();
+            var observations = new StringBuilder();
+
+            subject.Subscribe(s => observations.Append(s));
+            subject.OnNext("a");
+            subject.OnNext("b");
+
+            Assert.That(observations.ToString(), Is.Empty);
+        }
+
+        [Test]
+        public void AsyncSubjectCompleted()
+        {
+            var subject = new AsyncSubject<string>();
+            var observations = new StringBuilder();
+
+            subject.Subscribe(s => observations.Append(s));
+            subject.OnNext("a");
+            subject.OnNext("b");
+            subject.OnCompleted();
+
+            Assert.That(observations.ToString(), Is.EqualTo("b"));
+        }
+
+        [Test]
+        public void UseFactoryCorrectly()
+        {
+            var observable = Observable.Create<string>(o =>
+                {
+                    o.OnNext("a");
+                    o.OnNext("b");
+                    o.OnNext("c");
+                    o.OnCompleted();
+                    return Disposable.Empty;
+                });
+            var observations = new StringBuilder();
+            observable.Subscribe(s => observations.Append(s));
+
+            Assert.That(observations.ToString(), Is.EqualTo("abc"));
+        }
+    }
+}
---
+++ RxInvestigation/SequenceBasics.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace RxInvestigation
+{
+    [TestFixture]
+    class SequenceBasics
+    {
+        [Test]
+        public void Generators()
+        {
+            //var range = Observable.Range(10, 15);
+            var range = Observable.Generate(10, value => value < 25, value => value + 1, value => value);
+            var observations = new List<int>();
+            range.Subscribe(observations.Add);
+
+            Assert.That(observations.Count, Is.EqualTo(15));
+            Assert.That(observations[14], Is.EqualTo(24));
+        }
+
+        [Test]
+        public void TimerExample()
+        {
+            var timer = Observable.Timer(TimeSpan.FromMilliseconds(250));
+            timer.Subscribe(Console.WriteLine, () => Console.WriteLine("Completed"));
+        }
+
+        [Test]
+        public void StartExample()
+        {
+            var start = Observable.Start(() =>
+                {
+                    Console.WriteLine("Working away");
+                    for (var i = 0; i < 10; i++)
+                    {
+                        Thread.Sleep(100);
+                        Console.WriteLine(i);
+                    }
+                    return "published value";
+                });
+            start.Subscribe(Console.WriteLine, () => Console.WriteLine("Action completed"));
+
+            Thread.Sleep(1100);
+        }
+    }
+}
